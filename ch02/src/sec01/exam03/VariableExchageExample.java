9ec95c8a029d5414c1d44fa61a0dd20b477fc589
kukudass130 <tldhswls123@naver.com>
2024-01-13 20:39:19 +0900

    ch02_sec01 first commit

---
+++ ch02/src/sec01/exam03/VariableExchageExample.java
@@ -0,0 +1,18 @@
+package sec01.exam03;
+
+public class VariableExchageExample {
+
+	public static void main(String[] args) {
+		
+		int x = 3;
+		int y = 5;
+		System.out.println("x: "+ x + ", y: "+ y);
+		
+		int tmp= x;
+		x = y;
+		y = tmp;
+		System.out.println("x: "+ x + ", y: "+ y);
+
+	}
+
+}
